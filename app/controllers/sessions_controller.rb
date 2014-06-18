44ed1c7240639ef38e1553b92afc70c31eaf1f72
Bryce Kerley <bkerley@brycekerley.net>
2014-06-18 09:40:23 -0400

    logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.
+      find_by(username: params[:username]).
+      try(:authenticate, params[:password])
+
+    if @user
+      # logged in, hooray
+      redirect_to notes_path
+    else
+      render action: 'new'
+    end
+  end
 end
