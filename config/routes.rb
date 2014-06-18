8317c9d84ec52e175692913684d2a1222b457817
Bryce Kerley <bkerley@brycekerley.net>
2014-06-18 00:01:42 -0400

    spike out signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resource :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
